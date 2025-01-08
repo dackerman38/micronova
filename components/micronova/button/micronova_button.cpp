166976a0f6442aeea8d9e70c16f25241b5fa88a9
dackerman38 <dackerman38@gmail.com>
2025-01-08 21:23:35 +0100

    Update micronova_button.cpp

--- components/micronova/button/micronova_button.cpp
+++ components/micronova/button/micronova_button.cpp
@@ -14,5 +14,16 @@ void MicroNovaButton::press_action() {
   this->micronova_->update();
 }
 
+void MicroNovaButton1::press_action() {
+  switch (this->get_function()) {
+    case MicroNovaFunctions::STOVE_FUNCTION_CUSTOM:
+      this->micronova_->queue_write_request(this->memory_location_, this->memory_address_, this->memory_data_);
+      break;
+    default:
+      break;
+  }
+  this->micronova_->update();
+}
+
 }  // namespace micronova
 }  // namespace esphome
